db6db59dfdbd94330a5f80b288d936041d0d5bbd
yetisyellowyacht <mugofpunch@gmail.com>
2017-03-29 18:34:38 +0300

    Polished report format.

--- mysql_internal/proc_out_stat.sql
+++ mysql_internal/proc_out_stat.sql
@@ -6,6 +6,7 @@
 #   1.  Get the latest value from zabbix.history#
 #   2.  First show abnormal items               #
 #   3.  Then show the whole stats table         #
+#   4.  Then print out report summary           #
 #===============================================#
 
 USE reporter;
@@ -17,6 +18,7 @@ BEGIN
 # 0.
     DECLARE curtime INT;
     SET curtime = (SELECT max(rtime) FROM sum);
+    SELECT from_unixtime(curtime) AS "Report generated at:", now() AS "Current values from Zabbix requested at:";
 
 # 1.
     UPDATE reporter.stat AS s, zabbix.history AS z
@@ -32,22 +34,47 @@ BEGIN
       AND z.clock > unix_timestamp(now()) - 60;
 
 # 2.
-    SELECT from_unixtime(curtime) AS "Report generated at:";
+    SELECT "Max - Min > 3 dbm" AS `Warnings`;
     SELECT d.host, d.role, s.avg, s.dev, s.count, 
             s.min, s.max, s.dif, s.cur
         FROM def AS d, stat AS s
         WHERE d.itemid = s.itemid
         AND s.rtime = curtime
-        AND ...
-        ORDER BY d.role;
+        AND s.dif > 3
+        ORDER BY s.dif;
+
+    SELECT "Input too low" AS `Warnings`;
+    SELECT d.host, d.role, s.avg, s.dev, s.count, 
+            s.min, s.max, s.dif, s.cur
+        FROM def AS d, stat AS s
+        WHERE d.itemid = s.itemid
+        AND s.rtime = curtime
+        AND s.avg < -21
+        ORDER BY s.avg;
+
+    SELECT "Input too high" AS `Warnings`;
+    SELECT d.host, d.role, s.avg, s.dev, s.count, 
+            s.min, s.max, s.dif, s.cur
+        FROM def AS d, stat AS s
+        WHERE d.itemid = s.itemid
+        AND s.rtime = curtime
+        AND s.avg > -5
+        ORDER BY s.avg;
 
 # 3.
+    SELECT "Complete table" AS `And also...`;
     SELECT d.host, d.role, s.avg, s.dev, s.count, 
             s.min, s.max, s.dif, s.cur
         FROM def AS d, stat AS s
         WHERE d.itemid = s.itemid
         AND s.rtime = curtime
         ORDER BY d.role;
+
+# 4.
+    SELECT "Report on report" AS `Self-giagnostics`;
+    SELECT from_unixtime(rtime) as time, def, hist, stat
+        FROM sum
+        WHERE rtime = curtime;
 
 END//
 
