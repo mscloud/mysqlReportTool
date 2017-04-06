5ffa3bc1c6a52be98bad675160cc4ea274733ea7
yetisyellowyacht <mugofpunch@gmail.com>
2017-04-06 23:54:15 +0300

    Improved time calculations; Added filter condition for '-40.000000'; Updated comments.

--- mysql_internal/proc_pop_hist.sql
+++ mysql_internal/proc_pop_hist.sql
@@ -16,57 +16,85 @@ CREATE PROCEDURE pop_hist(
     INOUT p_status VARCHAR(8)
     )
 BEGIN
+
+# 0.
+# Exit procedure on warning, update status variable
+#
+# If any warnings are encountered during the procedure, a variable is assigned 
+# a special value, and the procedure is stopped. Then, report_master() procedure
+# writes this value into a summary table.
     DECLARE EXIT HANDLER FOR SQLWARNING
     BEGIN
         SET p_status = "Fail";
     END;
 
 # 1.
-    CREATE TABLE IF NOT EXISTS `hist` (                           # The `hist` table stores actual zabbix values taken 
-                                                                  # once per hour. It is a data source for `stat` table.
-        `itemid` bigint(20) unsigned NOT NULL,                    # Primary key is `itemid`+`htime`.
-        `htime` int(11) NOT NULL,                                 # 
-        `val` decimal(8,4) NOT NULL,                              # Pay attention to convert input values to decimal!
-        PRIMARY KEY `hi_1` (`itemid`,`htime`),                    # 
-        CONSTRAINT `hi_fk_1` FOREIGN KEY (`itemid`)               # `itemid` is a foreign key taken from `def`.
-            REFERENCES `def` (`itemid`) ON DELETE CASCADE         # 
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;                         # 
+# The `hist` table contains actual values for each item.
+# Before filling the table with data, we check that the table does exist.
+# 
+# Description of columns.
+# `itemid`: It is the fist part of the compound primary key for `hist`. 
+# It is also a foreign key which references to `def` table.
+# `htime`: A timestamp in unix format. It represents time at which measurement
+# was taken by zabbix.
+# `val`: value from zabbix.history translated to decimal type
+    CREATE TABLE IF NOT EXISTS `hist` (
+        `itemid` bigint(20) unsigned NOT NULL,
+        `htime` int(11) NOT NULL,
+        `val` decimal(8,4) NOT NULL,
+        PRIMARY KEY `hi_1` (`itemid`,`htime`),
+        INDEX `htime` (`htime`),
+        CONSTRAINT `hi_fk_1` FOREIGN KEY (`itemid`)
+            REFERENCES `def` (`itemid`) ON DELETE CASCADE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # 2.
-    INSERT IGNORE INTO reporter.hist (itemid, htime, val)         # IGNORE operator is to avoid duplicates.
-        SELECT h.itemid, h.clock, cast(h.value AS decimal(8,4))   # This query takes values from zabbix.history 
-                                                                  # (float values)
-            FROM zabbix.history AS h, reporter.def AS d           # Don't forget to query zabbix.history_str 
-                                                                  # for Medialinks!
-            WHERE h.itemid = d.itemid                             # 
-            AND d.role != 'skip'                                  # Some items need not to be processed
-            AND minute(from_unixtime(h.clock)) = 0                # Values selected on a per hour basis
-            AND h.clock > unix_timestamp(date_sub(now(),          # 
-                    interval 30 day))                             # Not older than 30 days
-            AND h.value > '-40.0'                                 # Drop LOS values which otherwise would spoil 
-    ;                                                             # statistics ('-Inf.', '-41', '-128')
+# In this block, zabbix history tables are processed and values for each item
+# in `def` are collected. To reduce amount of data, only one in each hour is
+# copied into `hist` table.
+# 
+# What should be noted is that two history tables are processed: zabbix.history
+# for float values, and zabbix.history_str for values that are stored as string
+# type (Medialinks is so special J).
+# 
+# General explanation for the block below.
+# 1) items from `def` which are marked as 'skip', are ignored;
+# 2) only values recorded at xx hours 00 minutes are inserted. this way,
+# sampling frequency is reduced from 1/min in zabbix to 1/hr in the report
+# generator;
+# 3) special values that represent loss of signal ('-128' and '-41' for Evertz,
+# '-40.0', '' and '-Inf' for Medialinks) are ignored;
+# 4) only values within last 30 days (2,592,000 seconds) are processed;
+# 5) zabbix.history and zabbix.history_str are processed consequently.
+#
+# IGNORE operator is used to avoid duplicate values when importing data from 
+# zabbix history. 
+    INSERT IGNORE INTO reporter.hist (itemid, htime, val)
+        SELECT h.itemid, h.clock, cast(h.value AS decimal(8,4))
+            FROM zabbix.history AS h, reporter.def AS d
+            WHERE h.itemid = d.itemid
+            AND d.role != 'skip'
+            AND minute(from_unixtime(h.clock)) = 0
+            AND h.clock > (curtime - 2592000)
+            AND h.value > '-40.0';
 
-    INSERT IGNORE INTO reporter.hist (itemid, htime, val)         # IGNORE operator is to avoid duplicates.
-        SELECT h.itemid, h.clock, cast(h.value AS decimal(8,4))   # This query takes values from 
-                                                                  # zabbix.history_str (string values) and 
-                                                                  # converts them to decimal type. Because 
-                                                                  # Medialinks MIB is so special J
-            FROM zabbix.history_str AS h, reporter.def AS d       # 
-            WHERE h.itemid = d.itemid                             # 
-            AND d.role != 'skip'                                  # Some items need not to be processed
-            AND minute(from_unixtime(h.clock)) = 0                # Values selected on a per hour basis
-            AND h.clock > unix_timestamp(date_sub(now(),          # 
-                    interval 30 day))                             # Not older than 30 days
-            AND h.value != '-Inf'                                 # Drop LOS values which otherwise would spoil 
-            AND h.value != ''                                     # statistics ('-Inf', '', '-41', '-128')
-    ;                                                             # 
+    INSERT IGNORE INTO reporter.hist (itemid, htime, val)
+        SELECT h.itemid, h.clock, cast(h.value AS decimal(8,4))
+            FROM zabbix.history_str AS h, reporter.def AS d
+            WHERE h.itemid = d.itemid
+            AND d.role != 'skip'
+            AND minute(from_unixtime(h.clock)) = 0
+            AND h.clock > (curtime - 2592000)
+            AND h.value NOT IN ('-Inf', '-40.000000', '') 
 
 # 3.
-    DELETE FROM hist                                              # Delete lines older than 30 days.
-        WHERE htime < unix_timestamp(date_sub(now(),              # 
-                interval 30 day));                                #
+# Remove values that are older than 30 days (2,592,000 seconds).
+    DELETE FROM hist
+        WHERE htime < (curtime - 2592000);
 
 # 4.
+# If the procedure didn't stop abnormally, we update the status variable, which
+# is then written into a summary table by report_master procedure.
     SET p_status = "OK";
 END//
 
