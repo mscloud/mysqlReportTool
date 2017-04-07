2c2c08c4edf924f5f733d450f6724880315a9a86
yetisyellowyacht <mugofpunch@gmail.com>
2017-04-07 23:42:44 +0300

    Updated comments.

--- mysql_internal/proc_master.sql
+++ mysql_internal/proc_master.sql
@@ -2,7 +2,7 @@
 
 #===============================================#
 #       Superprocedure                          #
-#   0.  Initialize variables                    #
+#   0.  Declare variables                       #
 #   1.  Call open_sum()                         #
 #   2.  Call pop_def()                          #
 #   3.  Call pop_hist()                         #
@@ -16,47 +16,67 @@ DELIMITER //
 CREATE PROCEDURE report_master()
 BEGIN
 
+# 0. 
+# curtime is a timestamp in unix format that will be used throughout the report
+# process.
+# p_status is a variable into which all procedures write their status.
     DECLARE curtime INT;
     DECLARE p_status VARCHAR(8);
     SET curtime = unix_timestamp(now());
-    SET p_status = "unknown";
 
+# 1.
+# This procedure adds a new line to a summary table and records the timestamp.
     CALL open_sum(curtime);
 
+# 2.
+# Print status message.
+# Reset the status variable.
+# Call a procedure.
+# Write status into a summary table.
+#   Success:            OK
+#   Warning:            Fail
+#   Procedure stopped:  unknown
     SELECT now() AS time, "Updating items list..." AS status;
+    SET p_status = "unknown";
     CALL pop_def(curtime, p_status);
     UPDATE sum 
         SET def = p_status
         WHERE rtime = curtime;
-    SET p_status = "unknown";
 
+# 3.
+# Exactly the same as previous block.
     SELECT now() AS time, "Updating values history..." AS status;
+    SET p_status = "unknown";
     CALL pop_hist(curtime, p_status);
     UPDATE sum 
         SET hist = p_status
         WHERE rtime = curtime;
-    SET p_status = "unknown";
 
+# 4.
+# Exactly the same as two previous blocks.
     SELECT now() AS time, "Calculating statistics..." AS status;
+    SET p_status = "unknown";
     CALL pop_stat(curtime, p_status);
     UPDATE sum 
         SET stat = p_status
         WHERE rtime = curtime;
 
+# 5.
+# Print the last line from the summary table.
 SELECT * FROM sum WHERE rtime = curtime;
 END//
 
 
 # appendix.
-CREATE EVENT daily_report
-    ON SCHEDULE EVERY 1 DAY
-    STARTS '2017-03-30 03:30:00' ENABLE
-    DO
-        BEGIN
-            CALL report_master();
-        END//
+-- CREATE EVENT daily_report
+--     ON SCHEDULE EVERY 1 DAY
+--     STARTS '2017-03-30 03:30:00' ENABLE
+--     DO
+--         BEGIN
+--             CALL report_master();
+--         END//
 
 DELIMITER ;
 
 # set global variable to enable scheduled events
-SET GLOBAL event_scheduler = ON;
+-- SET GLOBAL event_scheduler = ON;
--- mysql_internal/proc_open_sum.sql
+++ mysql_internal/proc_open_sum.sql
@@ -14,6 +14,21 @@ CREATE PROCEDURE open_sum(IN curtime INT)
 BEGIN
 
 # 1.
+# The `sum` table keeps results of execution of each subroutine.
+# Before filling the table with data, we check that the table does exist.
+# 
+# Description of columns.
+# `id`: auto-incremented numeric id for each report, the primary key.
+# `rtime`: A timestamp in unix format. It represents time at which report was
+# created.
+# `def`: result of pop_def(...)
+# `hist`: result of pop_hist(...)
+# `stat`: result of pop_stat(...)
+# these columns have 4 possible values:
+#   'OK'        - procedure was called and finished successfully;
+#   'Fail'      - procedure was called and exited by handler (warning);
+#   'unknown'   - procedure was not called or finished abnormally(major);
+#   'not set'   - update command was not executed (critical problem);
     CREATE TABLE IF NOT EXISTS `sum` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
         `rtime` int(11) NOT NULL,
@@ -25,12 +40,16 @@ BEGIN
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # 2.
+# Initialize new line. id is generated automatically, rtime is written, other
+# columns have default values.
     INSERT INTO sum (rtime) 
         VALUE (curtime);
 
 # 3.
+# Cleanup. Records older than one year (365 days = 365*24*3600 = 31 536 000)
+# are removed.
     DELETE FROM sum 
-        WHERE rtime < (curtime - 31536000);   # 365*24*3600 = 31 536 000
+        WHERE rtime < (curtime - 31536000);
 
 END//
 
