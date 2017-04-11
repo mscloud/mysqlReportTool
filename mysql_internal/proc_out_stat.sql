1dceb1209413b6bff38dcfdf236bdffdcba27c4b
yetisyellowyacht <mugofpunch@gmail.com>
2017-04-12 00:13:35 +0300

    Updated out_stat according to the to-do list.

--- mysql_internal/proc_out_stat.sql
+++ mysql_internal/proc_out_stat.sql
@@ -3,10 +3,9 @@
 #===============================================#
 #       Print stats and warnings                #
 #   0.  Get the latest report time              #
-#   1.  Get the latest value from zabbix.history#
-#   2.  First show abnormal items               #
-#   3.  Then show the whole stats table         #
-#   4.  Then print out report summary           #
+#   1.  Print warnings                          #
+#   2.  Then show the whole stats table         #
+#   3.  Then print out report summary           #
 #===============================================#
 
 USE reporter;
@@ -16,65 +15,56 @@ CREATE PROCEDURE out_stat()
 BEGIN
 
 # 0.
-    DECLARE curtime INT;
-    SET curtime = (SELECT max(rtime) FROM sum);
-    SELECT from_unixtime(curtime) AS "Report generated at:", now() AS "Current values from Zabbix requested at:";
+    DECLARE tstamp INT;
+    SET tstamp = (SELECT max(rtime) FROM sum);
+    SELECT from_unixtime(tstamp) AS "Report generated at:", now() AS "Current values from Zabbix requested at:";
 
 # 1.
-    UPDATE reporter.stat AS s, zabbix.history AS z
-      SET s.cur = cast(z.value as decimal(5,2))
-      WHERE s.itemid = z.itemid
-      AND s.rtime = curtime
-      AND z.clock > unix_timestamp(now()) - 60;
-    
-    UPDATE reporter.stat AS s, zabbix.history_str AS z
-      SET s.cur = cast(z.value as decimal(5,2))
-      WHERE s.itemid = z.itemid
-      AND s.rtime = curtime
-      AND z.clock > unix_timestamp(now()) - 60;
-
-# 2.
-    SELECT "Max - Min > 3 dbm" AS `Warnings`;
-    SELECT d.host, d.role, s.avg, s.dev, s.count, 
-            s.min, s.max, s.dif, s.cur
+    SELECT "avg1 - avg30 < -3 dbm" AS `Warnings`;
+    SELECT 
+            d.host, d.role, s.freq30, s.freq3, 
+            s.avg30, s.avg3, s.avg1, s.dif1, s.avg3dif
         FROM def AS d, stat AS s
         WHERE d.itemid = s.itemid
-        AND s.rtime = curtime
-        AND s.dif > 3
-        ORDER BY s.dif;
-
-    SELECT "Input too low" AS `Warnings`;
-    SELECT d.host, d.role, s.avg, s.dev, s.count, 
-            s.min, s.max, s.dif, s.cur
-        FROM def AS d, stat AS s
-        WHERE d.itemid = s.itemid
-        AND s.rtime = curtime
-        AND s.avg < -21
-        ORDER BY s.avg;
-
-    SELECT "Input too high" AS `Warnings`;
-    SELECT d.host, d.role, s.avg, s.dev, s.count, 
-            s.min, s.max, s.dif, s.cur
-        FROM def AS d, stat AS s
-        WHERE d.itemid = s.itemid
-        AND s.rtime = curtime
-        AND s.avg > -5
-        ORDER BY s.avg;
-
-# 3.
-    SELECT "Complete table" AS `And also...`;
-    SELECT d.host, d.role, s.avg, s.dev, s.count, 
-            s.min, s.max, s.dif, s.cur
-        FROM def AS d, stat AS s
-        WHERE d.itemid = s.itemid
-        AND s.rtime = curtime
+        AND s.rtime = tstamp
+        AND s.avg1 < s.avg30 - 3
         ORDER BY d.role;
 
-# 4.
+    SELECT "Input too low" AS `Warnings`;
+    SELECT 
+            d.host, d.role, s.freq30, s.freq3, 
+            s.avg30, s.avg3, s.avg1, s.dif1, s.avg3dif
+        FROM def AS d, stat AS s
+        WHERE d.itemid = s.itemid
+        AND s.rtime = tstamp
+        AND s.avg1 < -21
+        ORDER BY s.avg1;
+
+    SELECT "Input too high" AS `Warnings`;
+    SELECT 
+            d.host, d.role, s.freq30, s.freq3, 
+            s.avg30, s.avg3, s.avg1, s.dif1, s.avg3dif
+        FROM def AS d, stat AS s
+        WHERE d.itemid = s.itemid
+        AND s.rtime = tstamp
+        AND s.avg1 > -4
+        ORDER BY s.avg1;
+
+# 2.
+    SELECT "Complete table" AS `And also...`;
+    SELECT 
+            d.host, d.role, s.freq30, s.freq3, 
+            s.avg30, s.avg3, s.avg1, s.dif1, s.avg3dif
+        FROM def AS d, stat AS s
+        WHERE d.itemid = s.itemid
+        AND s.rtime = tstamp
+        ORDER BY d.role;
+
+# 3.
     SELECT "Report on report" AS `Self-giagnostics`;
     SELECT from_unixtime(rtime) as time, def, hist, stat
         FROM sum
-        WHERE rtime = curtime;
+        WHERE rtime = tstamp;
 
 END//
 
