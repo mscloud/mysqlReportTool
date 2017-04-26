a80b8e573a259a0a2741e6a61e6a09ffd86d2900
yetisyellowyacht <mugofpunch@gmail.com>
2017-04-27 02:51:49 +0300

    .

--- php_drafts/rep_dbaccess.php
+++ php_drafts/rep_dbaccess.php
@@ -1,6 +1,10 @@
 <?php
 // rep_dbaccess.php
-    $hn = 'localhost';
-    $un = 'phpclient';
-    $pw = 'pI-IpcIient';
-    $db = 'reporter';
+$hn = 'localhost';
+$un = 'phpclient';
+$pw = 'pI-IpcIient';
+$db = 'reporter';
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) 
+    die ("MySQL connection error: $conn->connect_error");
