811321812dec02986e201ed1e2f542fa8810dfc1
yetisyellowyacht <mugofpunch@gmail.com>
2017-04-27 02:52:48 +0300

    added item configuration feature.

--- php_drafts/rep_main.php
+++ php_drafts/rep_main.php
@@ -1,16 +1,22 @@
 <?php
 // rep_main.php
 
+// code start
 
-    // code start
+$got    = filter_input_array(INPUT_GET,  ['p' => NULL], true);
+$posted = filter_input_array(INPUT_POST, ['newrole' => NULL, 'itemid' => NULL], true);
+$posted['newrole'] = preg_replace("/[^a-z0-9!?:_ ]/i", "_", $posted['newrole']);
+print_r($got);
+print_r($posted);
+switch ($got['p']) {
+    case 'cfg': 
+        my_edit_items($conn, $posted);
+        break;
+    default: 
+        echo "Invalid address";
+        break;
+}
 
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if ($conn->connect_error) {
-        die ($conn->connect_error);
-    } else {
-        echo 'connection is up</br>';
-    }
-    
-    
 
-    // code end
+
+// code end
